5fc40ac840c9e78c5c056bc832186abf35cd7048
Pau Riera <pau.riera.forteza@gmail.com>
2020-04-14 17:33:03 +0200

    Second docker production test

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,7 +7,7 @@ fi
 
 domains=(musicbucket.net www.musicbucket.net)
 rsa_key_size=4096
-data_path="./nginx/certbot"
+data_path="./docker/nginx/certbot"
 email="" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
