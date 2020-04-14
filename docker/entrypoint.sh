bbdc625866f19298137e5ffb64ab9b235a1e1a42
Pau Riera <pau.riera.forteza@gmail.com>
2020-04-14 20:47:24 +0200

    Sixth docker production test

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-mkdir -p /data/static && mkdir -p /data/media && \
-    echo "Compiling messages..." && \
-    CACHE_TYPE=dummy SECRET_KEY=musicbucket python manage.py compilemessages && \
-    echo "Compressing..." && \
-    CACHE_TYPE=dummy SECRET_KEY=musicbucket python manage.py compress --traceback --force && \
-    echo "Collecting statics..." && \
-    CACHE_TYPE=dummy SECRET_KEY=musicbucket python manage.py collectstatic --noinput --traceback -v 0 && \
-    chmod -R 777 /data/
 
+echo "Applying database migrations..." && \
+    CACHE_TYPE=dummy SECRET_KEY=musicbucket python manage.py migrate
 
 gunicorn --bind :8000 main.wsgi:application
