642ad17eb60b180b0c8cf847cde8ba3a227ccfd1
Pau Riera <pau.riera.forteza@gmail.com>
2021-04-27 17:32:24 +0200

    Added created_at to TelegramUser and TelegramChat admin's list_display

--- src/telegram/admin.py
+++ src/telegram/admin.py
@@ -6,20 +6,20 @@ from telegram.models import TelegramUser, TelegramChat, SentSpotifyLink
 
 @admin.register(TelegramUser)
 class TelegramUserAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'link',)
+    list_display = ('__str__', 'link', 'created_at',)
     search_fields = ('username', 'first_name',)
     inlines = (LastfmUserInline,)
 
 
 @admin.register(TelegramChat)
 class TelegramChatAdmin(admin.ModelAdmin):
-    list_display = ('name', 'chat_type',)
+    list_display = ('name', 'chat_type', 'created_at',)
     list_filter = ('chat_type',)
     search_fields = ('name',)
 
 
 @admin.register(SentSpotifyLink)
 class SentSpotifyLinkAdmin(admin.ModelAdmin):
-    list_display = ['__str__', 'sent_by', 'chat', 'sent_at']
+    list_display = ('__str__', 'sent_by', 'chat', 'sent_at',)
     list_filter = ('link__link_type', 'chat',)
     search_fields = ('chat__name', 'sent_by__name', 'link__artist__name', 'link__album__name', 'link__track__name')
