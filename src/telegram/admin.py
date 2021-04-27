babe3bc84943d870b6c6dd5dde4cf12ee1d346c8
Pau Riera <pau.riera.forteza@gmail.com>
2021-04-27 17:28:30 +0200

    Added few Telegram Model fields in Django Admin. Added deploy.sh file

--- src/telegram/admin.py
+++ src/telegram/admin.py
@@ -6,15 +6,20 @@ from telegram.models import TelegramUser, TelegramChat, SentSpotifyLink
 
 @admin.register(TelegramUser)
 class TelegramUserAdmin(admin.ModelAdmin):
-    inlines = [LastfmUserInline, ]
+    list_display = ('__str__', 'link',)
+    search_fields = ('username', 'first_name',)
+    inlines = (LastfmUserInline,)
 
 
 @admin.register(TelegramChat)
 class TelegramChatAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'chat_type',)
+    list_filter = ('chat_type',)
+    search_fields = ('name',)
 
 
 @admin.register(SentSpotifyLink)
 class SentSpotifyLinkAdmin(admin.ModelAdmin):
-    list_display = ['__str__', 'sent_at']
-    list_filter = ('link__link_type',)
+    list_display = ['__str__', 'sent_by', 'chat', 'sent_at']
+    list_filter = ('link__link_type', 'chat',)
+    search_fields = ('chat__name', 'sent_by__name', 'link__artist__name', 'link__album__name', 'link__track__name')
