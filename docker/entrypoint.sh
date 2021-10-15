00bbbab2c88e33eb926cbc9eba748debe1b776cf
Pau Riera <pau.riera.forteza@gmail.com>
2021-10-15 11:20:36 +0200

    Increased gunicorn timeout

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,4 +4,4 @@
 echo "Applying database migrations..." && \
     CACHE_TYPE=dummy SECRET_KEY=musicbucket python manage.py migrate
 
-gunicorn --bind :8000 main.wsgi:application
+gunicorn --timeout 120 --bind :8000 main.wsgi:application
