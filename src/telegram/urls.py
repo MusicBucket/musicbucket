58f1dcb4c54bb0d6e7cc452f2a505922379c082e
Pau Riera <priera@bmat.com>
2022-06-14 10:08:32 +0200

    Removed Telegram Login

--- src/telegram/urls.py
+++ src/telegram/urls.py
@@ -1,12 +1,3 @@
-from django.urls import path
-from telegram import views
-
 app_name = "telegram"
 
-urlpatterns = [
-    path(
-        "login-callback/",
-        views.TelegramLoginCallbackView.as_view(),
-        name="login-callback",
-    )
-]
+urlpatterns = []
